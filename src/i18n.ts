5de55f5f57f21e48b3a5caae50515509f40b5092
Harris TIWA <57524148+TIWA-HARRIS-RD@users.noreply.github.com>
2025-08-06 12:29:00 +0100

    MAJ en.json

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,6 +5,10 @@ const locales = [
   { code: "ar", name: "العربية", flag: "🇸🇦" },
   { code: "pt", name: "Português", flag: "🇵🇹" },
   { code: "zh", name: "中文", flag: "🇨🇳" },
+  { code: "de", name: "Deutsch", flag: "🇩🇪" },
+  { code: "it", name: "Italiano", flag: "🇮🇹" },
+  { code: "ja", name: "日本語", flag: "🇯🇵" },
+  { code: "sw", name: "Swahili", flag: "🇸🇼" },
 ];
 
 export default locales;
